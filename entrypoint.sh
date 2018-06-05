800a6c90a3c20f89e7896f1389f1f4f76d589d2e
joemarct <joemar.ct@gmail.com>
2018-06-05 12:06:30 +0800

    Fixed errors and warning when starting docker-compose
    - Resolved warnings related to root as user
    - Resolved duplicate key error (ref:#46)
    - Resolved warning on searching for supervisord config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /code && python manage.py collectstatic --noinput
 cd /code && python manage.py migrate
-cd /code && echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'default2018')" | python manage.py shell
 cd /code && python manage.py loaddata venue/fixtures/*.json
 exec "$@"
