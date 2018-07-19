5956a846598a8b749779a39de3e3ce08041e416c
joemarct <joemar.ct@gmail.com>
2018-07-19 22:20:00 +0800

    Completed tests for crediting of points upon saving a forum post

--- venue/tests/points_tests.py
+++ venue/tests/points_tests.py
@@ -1,4 +1,5 @@
-from venue.models import UserProfile, ForumProfile, ForumPost, ForumSite
+from venue.models import UserProfile, ForumProfile, ForumPost, \
+    ForumSite, ForumUserRank
 from django.test import TestCase
 from django.utils import timezone
 from constance import config
@@ -7,11 +8,13 @@ from model_mommy import mommy
 from model_mommy.recipe import Recipe
 
 
-class PointsCreditTest(TestCase):
+class PointsCreditingTest(TestCase):
+    """ Tests the crediting of points that happens after a new forum
+    post is saved """
 
     def setUp(self):
         # Create a mock forum site
-        self.forum_site = mommy.make(ForumSite)
+        forum_site = mommy.make(ForumSite)
         # Create a mock user profile
         self.user_profile = mommy.make(UserProfile)
         # Create a mock forum rank
@@ -24,8 +27,8 @@ class PointsCreditTest(TestCase):
         # Create a mock forum profile
         forum_profile_recipe = Recipe(
             ForumProfile,
-            forum_rank=forum_rank,
-            user_profile=user_profile,
+            forum_rank=self.forum_rank,
+            user_profile=self.user_profile,
             forum=forum_site,
             forum_user_id='172792'
         )
@@ -38,19 +41,37 @@ class PointsCreditTest(TestCase):
             'timestamp': timezone.now()
         }
 
-    @mock.patch.object(ForumPost, 'save')
-    def test_forum_post_save(self, forum_post_save)
+    def test_points_crediting(self):
         # Create a forum post
-        self.forum_post = ForumPost(
-            user_profile=user_profile,
-            forum_profile=forum_profile,
-            forum_rank=forum_profile.forum_rank,
-            topic_id=post['topic_id'],
-            message_id=post['message_id'],
-            unique_content_length=post['content_length'],
-            timestamp=post['timestamp']
+        forum_post = ForumPost(
+            user_profile=self.user_profile,
+            forum_profile=self.forum_profile,
+            forum_rank=self.forum_profile.forum_rank,
+            topic_id=self.post['topic_id'],
+            message_id=self.post['message_id'],
+            unique_content_length=self.post['content_length'],
+            timestamp=self.post['timestamp']
         )
-        self.forum_post.save()
-        # Test with asserts
-        self.assetTrue(forum_post_save.save.called)
-        self.assertEqual(forum_post, config.POST_POINTS_MULTIPLIER)
+        forum_post.save()
+        # Check credited base points
+        self.assertEqual(forum_post.base_points, config.POST_POINTS_MULTIPLIER)
+        # Check credited bonus percentage
+        bonus_pct = self.forum_rank.bonus_percentage
+        self.assertEqual(forum_post.influence_bonus_pts, bonus_pct)
+        # Check credited bonus points
+        expected_bonus = config.POST_POINTS_MULTIPLIER * (bonus_pct/100)
+        self.assertEqual(forum_post.influence_bonus_pts, expected_bonus)
+        # Check credited total points
+        expected_total = config.POST_POINTS_MULTIPLIER + expected_bonus
+        self.assertEqual(forum_post.total_points, expected_total)
+
+
+class SignatureMinutesMonitoringTest(TestCase):
+    """ Tests the monitoring and categorization of signature minutes that
+    happen every time a forum post is saved """
+
+    def setUp(self):
+        pass
+
+    def test_sig_minutes_monitoring(self):
+        pass
