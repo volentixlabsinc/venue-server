b891c105339f2d8f4738dba2bfade6004a488992
Joemar Taganna <joemar.ct@gmail.com>
2018-08-31 12:23:28 +0800

    Fixed cause of failing test - None replaced with empty string

--- venue/scrapers/exceptions.py
+++ venue/scrapers/exceptions.py
@@ -3,6 +3,9 @@ class ProfileDoesNotExist(Exception):
     def __init__(self, message, info={}):
             self.message = message
             self.info = info
+            if 'response_text' in self.info.keys():
+                if self.info['response_text'] is None:
+                    self.info['response_text'] = ''
 
 
 class ScraperError(Exception):
