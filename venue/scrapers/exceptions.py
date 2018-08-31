4a3465ebfdb2416af7249993be2e1ecabfe994ff
Joemar Taganna <joemar.ct@gmail.com>
2018-08-31 15:56:40 +0800

    Catch and log connection errors, re-raise as ScraperError and retry
    ref:#226

--- venue/scrapers/exceptions.py
+++ venue/scrapers/exceptions.py
@@ -3,9 +3,6 @@ class ProfileDoesNotExist(Exception):
     def __init__(self, message, info={}):
             self.message = message
             self.info = info
-            if 'response_text' in self.info.keys():
-                if self.info['response_text'] is None:
-                    self.info['response_text'] = ''
 
 
 class ScraperError(Exception):
@@ -13,6 +10,3 @@ class ScraperError(Exception):
     def __init__(self, message, info={}):
             self.message = message
             self.info = info
-            if 'response_text' in self.info.keys():
-                if self.info['response_text'] is None:
-                    self.info['response_text'] = ''
